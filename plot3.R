199e497fa227f23261a2ef4e0ea728d0c8e86bed
Sateeshnallamothu <sknallamothu@hotmail.com>
2016-12-15 18:39:56 -0600

    third plot

--- plot3.R
+++ plot3.R
@@ -21,7 +21,7 @@ plot(hh.power.con$DateTime,hh.power.con$Sub_metering_1,type="l",
 points(hh.power.con$DateTime,hh.power.con$Sub_metering_2,type="l",col='red')
 points(hh.power.con$DateTime,hh.power.con$Sub_metering_3,type="l",col='blue')
 legend('topright',legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
-       col=c('black','red','blue'),lty=1,cex=0.5)
+       col=c('black','red','blue'),lty=1,cex=0.75)
 
 # write to file and close the device.
 dev.copy(png,file='ExData_Plotting1/plot3.png',width = 480, height = 480, units = "px")
